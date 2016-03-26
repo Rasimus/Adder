b4100d97b7df3b77c1ab832485de69fadf9068f0
KristoferSundequist <krajle@gmail.com>
2016-03-26 17:59:08 +0100

    Började på req2

--- ospp_2016_problem_set_3_erlang/src/add.erl
+++ ospp_2016_problem_set_3_erlang/src/add.erl
@@ -1,6 +1,6 @@
 %% @doc Erlang mini project.
 -module(add).
--export([start/3, start/4, getCarryOuts/4, addDigits/4]).
+-export([start/3, start/4, getCarryOuts/4, addDigits/4, addThis/4]).
 
 %% @doc TODO: add documentation
 -spec start(A,B,Base) -> ok when 
@@ -35,8 +35,43 @@ start(A,B, Base) ->
       Option::atom() | tuple(),
       Options::[Option].
 
+
+%TODO: Assumes A, B is same length: (prepend 0's to shorter int to solve this)
 start(A,B,Base, Options) ->
-    tbi.
+    ASubs = utils:split(integer_to_list(A), Options),
+    BSubs = utils:split(integer_to_list(B), Options),
+    Master = self(),
+    LastProcess = distribute(ASubs, BSubs, Base, 1, Master, Master),
+    LastProcess ! {carry, 0},
+    listener([]).
+
+
+%%Starts all process's and Returns last process so we can send carry=0 to it.
+distribute([A], [B], Base, Index, Next,Master) ->
+    spawn( fun() -> otherProcess(A, B, Base, Index, Next, Master) end);
+
+
+distribute([A|Ax], [B|Bx], Base, Index, Next,Master) ->
+    SpawnedProcess = spawn( fun() -> otherProcess(A, B, Base, Index, Next, Master) end),
+    distribute(Ax,Bx,Base,Index+1,SpawnedProcess,Master).
+
+
+otherProcess(A,B,Base, Index, Next, Master) ->
+    receive
+	{carry, CarryIn} ->
+	    {CarryOut, Sum} = addThis(list_to_integer(A),list_to_integer(B),Base,CarryIn),
+	    Master ! {sum, Sum, Index},
+	    Next ! {carry, CarryOut}
+    end.
+    
+listener(Sums) ->
+    receive
+	{sum, Sum, Index} ->
+	    listener([{Index,Sum}|Sums]);
+	{carry, CarryOut} ->
+	    {_,Sum} = lists:unzip([{0, CarryOut} | lists:keysort(1,Sums)]),
+	    lists:flatten(Sum)
+    end.
 
 
 %% @doc Creates a list of carry outs from an arithmetic addition.
@@ -74,3 +109,18 @@ addDigits(A, B, Base, Cin) ->
     Sum = (A + B + Cin) rem Base,
     Cout = (A + B + Cin) div Base,
     {Sum, Cout}.
+
+
+%Returns {COUT, SUM}::{int, int}
+addThis(A,B,Base,CIN) ->
+    {[SumHead|SumT], _} = lists:unzip(getCarryOuts(A,B,Base,CIN)),
+    LongestCount = length(integer_to_list(max(A,B))),
+    if
+	length([SumHead|SumT]) > LongestCount ->
+	    {SumHead, SumT};
+	true ->
+	    {0, [SumHead|SumT]}
+    end.
+	    
+	
+    
